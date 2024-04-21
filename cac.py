c4347c3ad6facb49739b5c91e7f545ed85372dc7
Ikechukwu <iyke497@gmail.com>
2024-04-21 18:06:12 +0100

    removed zst and cac.py

--- cac.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def get_company_rc(name):
-	res = requests.post(
-		url="https://searchapp.cac.gov.ng/searchapp/api/public-search/company-business-name-it",
-		data={"searchTerm": name})
-	return res.json()
-
-print(get_company_rc("ZST TECHNOLOGIES LIMITED"))
